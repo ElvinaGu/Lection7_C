dbc5175cc418bee6c8ff77947cb1c69832ad7b3f
Elvina <Elka121194@yandex.ru>
2023-04-26 18:01:17 +0300

    Recursion. Folder crawling

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Пройтись по всем файлам в папке
+
+// path - путь к текущей папке, indent - отступы.
+void CatalogInfo(string path, string indent = "")
+{
+  // Получаем информацию о директории.
+  DirectoryInfo catalog = new DirectoryInfo(path);
+
+  // Массив всех файлов, находящиеся в этой папке.
+  DirectoryInfo[] catalogs = catalog.GetDirectories();
+  for (int i = 0; i < catalogs.Length; i++)
+  {
+    Console.WriteLine($"{indent}{catalogs[i].Name}");
+    CatalogInfo(catalogs[i].FullName, indent + " ");
+  }
+  
+  // Список файлов текущей директории.
+  FileInfo[] files = catalog.GetFiles();
+  for (int i = 0; i < files.Length; i++)
+  {
+    Console.WriteLine($"{indent}{files[i].Name}");
+  }
+}
+
+string path = @"C:\Users\Ильнар\OneDrive\Рабочий стол\Education\C#\Lection7_C";
+CatalogInfo(path);
